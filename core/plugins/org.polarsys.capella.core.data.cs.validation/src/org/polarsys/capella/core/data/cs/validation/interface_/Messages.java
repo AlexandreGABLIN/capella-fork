ffcb145fa178c7fc0949012da3e3b8264e206d72
Alexandre GABLIN <alexandre.gablin@obeosoft.com>
2026-01-08 15:33:37 +0100

    Marked strings as non translatable + Externalized UI Strings

--- core/plugins/org.polarsys.capella.core.data.cs.validation/src/org/polarsys/capella/core/data/cs/validation/interface_/Messages.java
+++ core/plugins/org.polarsys.capella.core.data.cs.validation/src/org/polarsys/capella/core/data/cs/validation/interface_/Messages.java
@@ -22,6 +22,8 @@ public class Messages extends NLS {
   public static String ExchangeItemAllocationProtocolSequence_2;
   public static String ExchangeItemAllocationProtocolSequence_4;
   public static String ExchangeItemAllocationProtocolSequence_5;
+  public static String MDCHKComponentInterfaceFunctionPortExchangeItemAllocationConsistency_3;
+  public static String MDCHKComponentInterfaceFunctionPortExchangeItemAllocationConsistency_4;
   static {
     // initialize resource bundle
     NLS.initializeMessages(BUNDLE_NAME, Messages.class);
