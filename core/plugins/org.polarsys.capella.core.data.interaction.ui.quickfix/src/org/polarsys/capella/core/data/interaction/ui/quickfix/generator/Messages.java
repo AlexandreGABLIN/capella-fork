7fbfa548a73475ca6c86a51aeb77c4adaf74efdd
Alexandre GABLIN <alexandre.gablin@obeosoft.com>
2026-01-09 17:20:52 +0100

    Externalized UI Strings + Marked strings as non translatable

--- core/plugins/org.polarsys.capella.core.data.interaction.ui.quickfix/src/org/polarsys/capella/core/data/interaction/ui/quickfix/generator/Messages.java
+++ core/plugins/org.polarsys.capella.core.data.interaction.ui.quickfix/src/org/polarsys/capella/core/data/interaction/ui/quickfix/generator/Messages.java
@@ -6,12 +6,30 @@ public class Messages extends NLS {
 	private static final String BUNDLE_NAME = Messages.class.getPackageName() + ".messages"; //$NON-NLS-1$
 	public static String DWF_CA_07_Resolutions_0;
 	public static String DWF_CA_07_Resolutions_1;
+	public static String DWF_DS_04_Resolutions_0;
+	public static String DWF_DS_04_Resolutions_1;
+	public static String DWF_DS_04_Resolutions_2;
+	public static String DWF_DS_04_Resolutions_3;
+	public static String DWF_DS_04_Resolutions_4;
+	public static String DWF_DS_04_Resolutions_5;
+	public static String DWF_DS_18_Resolutions_0;
+	public static String DWF_DS_18_Resolutions_1;
+	public static String DWF_DS_18_Resolutions_2;
+	public static String DWF_DS_18_Resolutions_3;
+	public static String DWF_DS_18_Resolutions_4;
+	public static String DWF_DS_18_Resolutions_5;
+	public static String DWF_DS_18_Resolutions_6;
+	public static String DWF_DS_18_Resolutions_7;
 	public static String DWF_DS_19_Resolutions_0;
 	public static String DWF_DS_19_Resolutions_1;
 	public static String DWF_DS_19_Resolutions_2;
 	public static String DWF_DS_19_Resolutions_3;
 	public static String DWF_DS_19_Resolutions_4;
 	public static String DWF_DS_19_Resolutions_5;
+	public static String DWF_DS_21_Resolutions_0;
+	public static String DWF_DS_21_Resolutions_1;
+	public static String DWF_DS_21_Resolutions_2;
+	public static String Helper_Resolutions_1;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
