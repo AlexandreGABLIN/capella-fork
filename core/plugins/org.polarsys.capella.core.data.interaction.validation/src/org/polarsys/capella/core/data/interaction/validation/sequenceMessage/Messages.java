500f6c6c95f25485933ea12bc3034ddd20481876
Alexandre GABLIN <alexandre.gablin@obeosoft.com>
2026-01-12 10:21:54 +0100

    Externalized UI Strings

--- core/plugins/org.polarsys.capella.core.data.interaction.validation/src/org/polarsys/capella/core/data/interaction/validation/sequenceMessage/Messages.java
+++ core/plugins/org.polarsys.capella.core.data.interaction.validation/src/org/polarsys/capella/core/data/interaction/validation/sequenceMessage/Messages.java
@@ -4,6 +4,9 @@ import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = Messages.class.getPackageName() + ".messages"; //$NON-NLS-1$
+	public static String MDCHK_SequenceMessage_ES_OES_InvokedOperation_0;
+	public static String MDCHK_SequenceMessage_ES_OES_InvokedOperation_1;
+	public static String MDCHK_SequenceMessage_ES_OES_InvokedOperation_4;
 	public static String MDCHK_SequenceMessage_FS_OAS_InvokedOperation_0;
 	public static String MDCHK_SequenceMessage_FS_OAS_InvokedOperation_1;
 	public static String SequenceMessageInvokedOperationExchangeItems_1;
