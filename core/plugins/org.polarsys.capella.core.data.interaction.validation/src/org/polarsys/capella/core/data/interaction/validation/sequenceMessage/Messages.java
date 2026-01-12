3c4e10fd7ceb34a115a22efee6b21f82593318eb
Alexandre GABLIN <alexandre.gablin@obeosoft.com>
2026-01-12 09:51:38 +0100

    Marked strings as non translatable + Externalized UI Strings

--- core/plugins/org.polarsys.capella.core.data.interaction.validation/src/org/polarsys/capella/core/data/interaction/validation/sequenceMessage/Messages.java
+++ core/plugins/org.polarsys.capella.core.data.interaction.validation/src/org/polarsys/capella/core/data/interaction/validation/sequenceMessage/Messages.java
@@ -6,6 +6,7 @@ public class Messages extends NLS {
 	private static final String BUNDLE_NAME = Messages.class.getPackageName() + ".messages"; //$NON-NLS-1$
 	public static String MDCHK_SequenceMessage_FS_OAS_InvokedOperation_0;
 	public static String MDCHK_SequenceMessage_FS_OAS_InvokedOperation_1;
+	public static String SequenceMessageInvokedOperationExchangeItems_1;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
