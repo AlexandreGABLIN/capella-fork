6c71596b901932a0d95c81a0558f9bb6f8080f48
Alexandre GABLIN <alexandre.gablin@obeosoft.com>
2026-01-12 17:10:19 +0100

    Externalized UI Strings

--- core/plugins/org.polarsys.capella.core.model.helpers/src/org/polarsys/capella/core/model/helpers/move/MoveHelper.java
+++ core/plugins/org.polarsys.capella.core.model.helpers/src/org/polarsys/capella/core/model/helpers/move/MoveHelper.java
@@ -230,7 +230,7 @@ public class MoveHelper implements IMoveHelper {
 
         if (!isOK) {
           // We should explain why !
-          return new Status(IStatus.ERROR, "model.helpers", "Semantic rules failed.");
+          return new Status(IStatus.ERROR, "model.helpers", "Semantic rules failed."); //$NON-NLS-1$
         }
       }
     }
