a3e0c445fa38f76c3143bcb6199444908401ca42
Alexandre GABLIN <alexandre.gablin@obeosoft.com>
2026-01-12 10:36:57 +0100

    Externalized UI Strings

--- core/plugins/org.polarsys.capella.core.data.la.validation/src/org/polarsys/capella/core/data/la/validation/logicalComponent/Messages.java
+++ core/plugins/org.polarsys.capella.core.data.la.validation/src/org/polarsys/capella/core/data/la/validation/logicalComponent/Messages.java
@@ -4,6 +4,7 @@ import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = Messages.class.getPackageName() + ".messages"; //$NON-NLS-1$
+	public static String LogicalActor_RealizedSystemActor_0;
 	public static String MDCHK_LogicalComponent_Realization_1_0;
 	static {
 		// initialize resource bundle
