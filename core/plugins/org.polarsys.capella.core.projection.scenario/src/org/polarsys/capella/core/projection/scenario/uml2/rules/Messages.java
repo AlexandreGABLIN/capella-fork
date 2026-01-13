de286b09642d72eac203038ebdf6a89af9c1fac3
Alexandre GABLIN <alexandre.gablin@obeosoft.com>
2026-01-13 16:51:34 +0100

    Externalized UI Strings

--- core/plugins/org.polarsys.capella.core.projection.scenario/src/org/polarsys/capella/core/projection/scenario/uml2/rules/Messages.java
+++ core/plugins/org.polarsys.capella.core.projection.scenario/src/org/polarsys/capella/core/projection/scenario/uml2/rules/Messages.java
@@ -8,6 +8,7 @@ public class Messages extends NLS {
 	public static String FC2FSInitialization_1;
 	public static String FC2FSInitialization_3;
 	public static String FC2FSInitialization_4;
+	public static String Rule_StateFragment_0;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
