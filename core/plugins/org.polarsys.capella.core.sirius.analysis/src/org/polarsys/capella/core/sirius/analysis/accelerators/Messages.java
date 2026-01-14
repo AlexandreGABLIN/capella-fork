8bb3f0b51e8c6dba54168bc40fe7ac3a9e379877
Alexandre GABLIN <alexandre.gablin@obeosoft.com>
2026-01-14 09:52:14 +0100

    Externalized UI Strings + Marked strings as non translatable

--- core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/accelerators/Messages.java
+++ core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/accelerators/Messages.java
@@ -6,6 +6,14 @@ public class Messages extends NLS {
 	private static final String BUNDLE_NAME = Messages.class.getPackageName() + ".messages"; //$NON-NLS-1$
 	public static String AcceleratorOnSequenceLinkHandler_0;
 	public static String AcceleratorOnSequenceLinkHandler_1;
+	public static String SelectOrCreateFunctionalExchangeDialog_1;
+	public static String SelectOrCreateFunctionalExchangeDialog_2;
+	public static String SelectOrCreateFunctionalExchangeDialog_3;
+	public static String SelectOrCreateFunctionalExchangeDialog_4;
+	public static String SelectOrCreateFunctionalExchangeDialog_5;
+	public static String SelectOrCreateFunctionalExchangeDialog_6;
+	public static String SelectOrCreateFunctionalExchangeDialog_7;
+	public static String SelectOrCreateFunctionalExchangeDialog_8;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
