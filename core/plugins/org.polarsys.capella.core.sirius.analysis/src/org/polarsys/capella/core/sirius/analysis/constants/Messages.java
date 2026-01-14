aa123be3066a1b30e35f6c04de8683312867094d
Alexandre GABLIN <alexandre.gablin@obeosoft.com>
2026-01-14 09:59:05 +0100

    Marked strings as non translatable + Externalized UI Strings

--- core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/constants/Messages.java
+++ core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/constants/Messages.java
@@ -5,6 +5,8 @@ import org.eclipse.osgi.util.NLS;
 public class Messages extends NLS {
 	private static final String BUNDLE_NAME = Messages.class.getPackageName() + ".messages"; //$NON-NLS-1$
 	public static String ColorNameConstants_1;
+	public static String IFilterNameConstants_330;
+	public static String IFilterNameConstants_331;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
