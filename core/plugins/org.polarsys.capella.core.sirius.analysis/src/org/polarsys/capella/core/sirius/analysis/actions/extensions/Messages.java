453e64734d8558a56950d19cdc46adc1dc970ba0
Alexandre GABLIN <alexandre.gablin@obeosoft.com>
2026-01-14 09:54:07 +0100

    Externalized UI Strings + Marked strings as non translatable

--- core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/actions/extensions/Messages.java
+++ core/plugins/org.polarsys.capella.core.sirius.analysis/src/org/polarsys/capella/core/sirius/analysis/actions/extensions/Messages.java
@@ -20,6 +20,7 @@ import org.eclipse.osgi.util.NLS;
 public class Messages extends NLS {
   private static final String BUNDLE_NAME = "org.polarsys.capella.core.sirius.analysis.actions.extensions.messages"; //$NON-NLS-1$
   public static String SelectElementFromListWizard_Title;
+  public static String SelectElementsFromCheckBoxWizard_2;
   public static String SelectOrCreateFunctionalExchangeWizard_0;
   public static String SelectOrCreateFunctionalExchangeWizard_1;
   public static String SelectOrCreateFunctionalExchangeWizard_2;
