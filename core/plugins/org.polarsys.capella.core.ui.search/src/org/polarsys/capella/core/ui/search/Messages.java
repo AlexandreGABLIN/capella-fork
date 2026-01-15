b9aa7861964f3a32da9e91382736b2aec5778543
Alexandre GABLIN <alexandre.gablin@obeosoft.com>
2026-01-15 11:27:32 +0100

    Marked strings as non translatable + Externalized UI Strings

---
+++ core/plugins/org.polarsys.capella.core.ui.search/src/org/polarsys/capella/core/ui/search/Messages.java
@@ -0,0 +1,52 @@
+package org.polarsys.capella.core.ui.search;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = Messages.class.getPackageName() + ".messages"; //$NON-NLS-1$
+	public static String CapellaSearchConstants_1;
+	public static String CapellaSearchConstants_10;
+	public static String CapellaSearchConstants_11;
+	public static String CapellaSearchConstants_13;
+	public static String CapellaSearchConstants_14;
+	public static String CapellaSearchConstants_15;
+	public static String CapellaSearchConstants_16;
+	public static String CapellaSearchConstants_17;
+	public static String CapellaSearchConstants_18;
+	public static String CapellaSearchConstants_19;
+	public static String CapellaSearchConstants_23;
+	public static String CapellaSearchConstants_24;
+	public static String CapellaSearchConstants_25;
+	public static String CapellaSearchConstants_26;
+	public static String CapellaSearchConstants_27;
+	public static String CapellaSearchConstants_3;
+	public static String CapellaSearchConstants_30;
+	public static String CapellaSearchConstants_32;
+	public static String CapellaSearchConstants_33;
+	public static String CapellaSearchConstants_34;
+	public static String CapellaSearchConstants_35;
+	public static String CapellaSearchConstants_36;
+	public static String CapellaSearchConstants_37;
+	public static String CapellaSearchConstants_38;
+	public static String CapellaSearchConstants_39;
+	public static String CapellaSearchConstants_4;
+	public static String CapellaSearchConstants_40;
+	public static String CapellaSearchConstants_41;
+	public static String CapellaSearchConstants_42;
+	public static String CapellaSearchConstants_43;
+	public static String CapellaSearchConstants_44;
+	public static String CapellaSearchConstants_45;
+	public static String CapellaSearchConstants_46;
+	public static String CapellaSearchConstants_5;
+	public static String CapellaSearchConstants_6;
+	public static String CapellaSearchConstants_7;
+	public static String CapellaSearchConstants_8;
+	public static String CapellaSearchConstants_9;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
