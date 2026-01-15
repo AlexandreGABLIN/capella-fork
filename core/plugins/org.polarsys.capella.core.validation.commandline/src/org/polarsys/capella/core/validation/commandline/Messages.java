f3a2f61bb6081c8903121e198aae02474ed32174
Alexandre GABLIN <alexandre.gablin@obeosoft.com>
2026-01-15 11:41:29 +0100

    Marked strings as non translatable

--- core/plugins/org.polarsys.capella.core.validation.commandline/src/org/polarsys/capella/core/validation/commandline/Messages.java
+++ core/plugins/org.polarsys.capella.core.validation.commandline/src/org/polarsys/capella/core/validation/commandline/Messages.java
@@ -20,6 +20,7 @@ public class Messages extends NLS {
   private static final String BUNDLE_NAME = "org.polarsys.capella.core.validation.commandline.messages"; //$NON-NLS-1$
   public static String resource_prefix;
   public static String resultsFileName;
+  public static String ValidationCommandLine_2;
   public static String validationStatus;
   static {
     // initialize resource bundle
